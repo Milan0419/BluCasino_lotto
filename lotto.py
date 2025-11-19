26b7ff49c79771b059c541f2993c3380fd6adca0
Milan0419 <balint.milan@diak.szbi-pg.hu>
2025-11-19 11:20:52 +0100

    Create lotto.py

---
+++ lotto.py
@@ -0,0 +1,26 @@
+import random
+
+lotto = []
+for i in range(5):
+    i = random.randint(1, 99)
+    lotto.append(i)
+    
+print(lotto)
+
+szamok = []
+for i in range(5):
+    i = int(input("Írd be a lottószámod:"))
+    szamok.append(i)
+
+
+for i in range(5):
+    if lotto.count(szamok[i]) == 1:
+        print("Helyes szám:")
+        print(szamok[i])
+        
+
+
+print("A te számaid: ")
+print(*szamok)
+print('A helyes számok: ')
+print(*lotto)
